cfb330694736ebb2a01d35cdf0e18376ce8ec833
Chris Manson <chris@manson.ie>
2020-11-10 16:11:12 +0000

    add mirage config

---
+++ mirage/config.js
@@ -0,0 +1,41 @@
+export default function() {
+  this.db.loadData({
+    tags: [
+      {
+        id: 'first',
+        title: 'First',
+        postIds: ['1', '3']
+      },
+      {
+        id: 'ember',
+        title: 'Ember Content',
+        postIds: ['2']
+      }
+    ],
+    posts: [
+      {
+        id: '1',
+        title: 'My greeatest blog',
+        content: 'a masterpiece',
+        tagId: 'first',
+      },
+      {
+        id: '2',
+        title: 'An alright blog',
+        content: 'a masterpiece',
+        tagId: 'ember',
+      },
+      {
+        id: '3',
+        title: 'Rushed content',
+        content: 'a masterpiece',
+        tagId: 'first',
+      }
+    ],
+  });
+
+  this.get('/tags')
+  this.get('/tags/:id')
+  this.get('/posts');
+  this.get('/posts/:id');
+}
